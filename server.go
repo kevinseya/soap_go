b1abc249602f788e4160ce80a52324eff3f7938c
kevinseya <kevinseya@gmail.com>
2024-12-04 09:29:50 -0500

    fix: :bug: update server.go

--- server.go
+++ server.go
@@ -1,49 +1,74 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
 )
 
-func main() {
-	// Creamos el cuerpo XML para la solicitud SOAP
-	soapRequest := `<?xml version="1.0" encoding="UTF-8"?>
+// Definimos las estructuras para el mensaje SOAP
+type HelloRequest struct {
+	XMLName xml.Name `xml:"HelloRequest"`
+	Name    string   `xml:"name"`
+}
+
+type HelloResponse struct {
+	XMLName xml.Name `xml:"HelloResponse"`
+	Message string   `xml:"message"`
+}
+
+// Estructura para el Envelope SOAP
+type SOAPEnvelope struct {
+	XMLName xml.Name `xml:"Envelope"`
+	Body    SOAPBody `xml:"Body"`
+}
+
+// Estructura para el Body SOAP, que contiene el HelloRequest
+type SOAPBody struct {
+	HelloRequest HelloRequest `xml:"HelloRequest"`
+}
+
+// Esta función maneja las solicitudes SOAP
+func soapHandler(w http.ResponseWriter, r *http.Request) {
+	// Aseguramos que solo aceptamos solicitudes POST
+	if r.Method != "POST" {
+		http.Error(w, "Only POST method is supported", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Decodificamos el mensaje SOAP entrante
+	var envelope SOAPEnvelope
+	decoder := xml.NewDecoder(r.Body)
+	err := decoder.Decode(&envelope)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error leyendo el SOAP: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	// Creamos la respuesta
+	res := HelloResponse{
+		Message: fmt.Sprintf("¡Hola Mundo, %s!", envelope.Body.HelloRequest.Name),
+	}
+
+	// Preparamos la respuesta SOAP
+	response := fmt.Sprintf(`<?xml version="1.0" encoding="UTF-8"?>
 	<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://www.example.org/hello">
 		<soapenv:Header/>
 		<soapenv:Body>
-			<web:HelloRequest>
-				<web:name>Mundo</web:name>
-			</web:HelloRequest>
+			<web:HelloResponse>
+				<web:message>%s</web:message>
+			</web:HelloResponse>
 		</soapenv:Body>
-	</soapenv:Envelope>`
+	</soapenv:Envelope>`, res.Message)
 
-	// Hacemos la solicitud SOAP al servidor
-	url := "http://localhost:8080/hello"
-	req, err := http.NewRequest("POST", url, strings.NewReader(soapRequest))
-	if err != nil {
-		log.Fatalf("Error creando la solicitud: %v", err)
-	}
+	// Enviamos la respuesta SOAP
+	w.Header().Set("Content-Type", "text/xml")
+	w.Write([]byte(response))
+}
 
-	// Configuramos los encabezados HTTP
-	req.Header.Set("Content-Type", "text/xml")
-
-	// Enviamos la solicitud
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Error al enviar la solicitud SOAP: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// Leemos la respuesta
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Error al leer la respuesta SOAP: %v", err)
-	}
-
-	// Mostramos la respuesta en consola
-	fmt.Printf("Respuesta del servidor:\n%s\n", string(body))
+func main() {
+	http.HandleFunc("/hello", soapHandler) // Manejamos las solicitudes en la ruta /hello
+	fmt.Println("Servidor SOAP escuchando en http://localhost:8080/hello")
+	log.Fatal(http.ListenAndServe(":8080", nil)) // Iniciamos el servidor en el puerto 8080
 }
